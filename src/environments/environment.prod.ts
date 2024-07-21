b96c1e520140a3f6ae5a409f6c495e3c85bd8f3b
Clintonkjkj <36886225+Clintonkjkj@users.noreply.github.com>
2024-07-22 02:58:20 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: "AIzaSyClrmwRFb26fcTJP6Vz1jy8QBvehKhQx18",
-  authDomain: "dashboard-95b04.firebaseapp.com",
-  projectId: "dashboard-95b04",
-  storageBucket: "dashboard-95b04.appspot.com",
-  messagingSenderId: "821289794846",
-  appId: "1:821289794846:web:eaddb9a0c0356c9ed2dd0f"
+    apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
   },
   instagram_token: 'INSTA_TOKEN',
   stripe_token: 'STRIPE_TOKEN',
